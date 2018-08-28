6b4f28638b73099d0fc630bddbc214196f7bacb1
abgalphabetz <arthur.mw.yeung@gmail.com>
2018-08-28 13:53:33 +0800

    done quick sort inplace

--- src/puffer/kata/sorting/quick_sort.py
+++ src/puffer/kata/sorting/quick_sort.py
@@ -19,3 +19,31 @@ def quick_sort(given: Sequence):
 
     print(given, " -> ", lsorted, pivot, rsorted)
     return lsorted + [pivot] + rsorted
+
+
+def quick_sort_inplace(given: Sequence):
+    if len(given) <= 1:
+        return given
+
+    given_list = list(given)
+    pivot = given_list[0]
+    left, right = 1, len(given_list) - 1
+
+    while left <= right:
+        if given_list[left] >= pivot >= given_list[right]:
+            given_list[left], given_list[right] = given_list[right], given_list[left]
+            left += 1
+            right -= 1
+        else:
+            if given_list[left] < pivot:
+                left += 1
+
+            if given_list[right] > pivot:
+                right -= 1
+
+    if right > 0:
+        given_list[0], given_list[right] = given_list[right], given_list[0]
+
+    print(given_list)
+
+    return quick_sort_inplace(given_list[:right]) + [pivot] + quick_sort_inplace(given_list[right + 1:])
--- src/puffer/kata/sorting/tests/test_sortings.py
+++ src/puffer/kata/sorting/tests/test_sortings.py
@@ -2,13 +2,13 @@ import pytest
 
 from src.puffer.kata.sorting.insertion_sort import insertion_sort
 from src.puffer.kata.sorting.merge_sort import merge_sort
-from src.puffer.kata.sorting.quick_sort import quick_sort
+from src.puffer.kata.sorting.quick_sort import quick_sort, quick_sort_inplace
 from src.puffer.kata.sorting.selection_sort import selection_sort
 from src.puffer.kata.sorting.bubble_sort import bubble_sort
 
 
 @pytest.mark.parametrize("sorting_func", [
-    bubble_sort, selection_sort, insertion_sort, merge_sort, quick_sort
+    bubble_sort, selection_sort, insertion_sort, merge_sort, quick_sort, quick_sort_inplace
 ])
 def test_sorting_function(sorting_func):
     ints1 = (1, 4, 3, 2, 2, 5, 9, 8, 7)
